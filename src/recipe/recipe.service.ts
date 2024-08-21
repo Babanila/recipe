5e58b90a5cb4d30b72a00d5273646dd39a23e117
babanila <babanila@yahoo.com>
2024-08-22 00:59:42 +0200

    Use prisma for api endpoints

--- src/recipe/recipe.service.ts
+++ src/recipe/recipe.service.ts
@@ -7,23 +7,37 @@ import { PrismaService } from 'src/prisma/prisma.service';
 export class RecipeService {
   constructor(private readonly prisma: PrismaService) {}
 
+  // This action adds a new recipe
   create(createRecipeDto: CreateRecipeDto) {
-    return 'This action adds a new recipe';
+    return this.prisma.recipe.create({
+      data: createRecipeDto,
+    });
   }
 
-  findAll() {
-    return `This action returns all recipe`;
+  // This action returns all recipe
+  async findAll() {
+    return this.prisma.recipe.findMany();
   }
 
+  // This action returns a #${id} recipe
   findOne(id: number) {
-    return `This action returns a #${id} recipe`;
+    return this.prisma.recipe.findUnique({
+      where: { id },
+    });
   }
 
+  // This action updates a #${id} recipe
   update(id: number, updateRecipeDto: UpdateRecipeDto) {
-    return `This action updates a #${id} recipe`;
+    return this.prisma.recipe.update({
+      where: { id },
+      data: updateRecipeDto,
+    });
   }
 
+  // This action removes a #${id} recipe
   remove(id: number) {
-    return `This action removes a #${id} recipe`;
+    return this.prisma.recipe.delete({
+      where: { id },
+    });
   }
 }
